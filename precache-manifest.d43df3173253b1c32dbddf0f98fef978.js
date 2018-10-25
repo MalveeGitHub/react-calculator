8c424836efd6997e3a7173bb378ed3387d4575ee
alvee <you@example.com>
2018-10-25 11:23:12 -0400

    Updates

---
+++ precache-manifest.d43df3173253b1c32dbddf0f98fef978.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2ecf2e68de309a6bd2b",
+    "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
+  },
+  {
+    "revision": "813c7e1704290ae7f0fb",
+    "url": "/react-calculator/static/js/main.813c7e17.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-calculator/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "813c7e1704290ae7f0fb",
+    "url": "/react-calculator/static/css/main.898f7ae0.chunk.css"
+  },
+  {
+    "revision": "0d01c1555db930bff768717f81f82786",
+    "url": "/react-calculator/index.html"
+  }
+];
